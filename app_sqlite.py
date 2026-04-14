31f4f2f8f8c70bcc3a0d09310bd1bbd94c8a1795
shivanikanugu08 <shivanikanugu@gmail.com>
2026-04-14 15:52:53 +0530

    ready for deployment

--- app_sqlite.py
+++ app_sqlite.py
@@ -1019,3 +1019,8 @@ if __name__ == '__main__':
                 print(f"Failed to recreate database: {e2}")
     
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+    import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
